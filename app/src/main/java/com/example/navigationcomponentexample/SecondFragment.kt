3769a39ce60eb38259bef03e6fe4832cbe7b45a7
byTurker <mr_turker@hotmail.com>
2021-08-12 11:13:35 +0300

    Init Navigation functions

---
+++ app/src/main/java/com/example/navigationcomponentexample/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.navigationcomponentexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.navigationcomponentexample.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+    private lateinit var binding : FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second, container, false)
+        return binding.root
+    }
+
+
+}
