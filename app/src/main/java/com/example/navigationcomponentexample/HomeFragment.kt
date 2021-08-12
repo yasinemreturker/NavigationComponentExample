2e56b981c48209fe59ee488a1928d09af0334387
byTurker <mr_turker@hotmail.com>
2021-08-12 11:26:59 +0300

    Transforming data between destinations

--- app/src/main/java/com/example/navigationcomponentexample/HomeFragment.kt
+++ app/src/main/java/com/example/navigationcomponentexample/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.navigationcomponentexample
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.navigationcomponentexample.databinding.FragmentHomeBinding
@@ -21,7 +24,12 @@ class HomeFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home,container,false)
 
         binding.button.setOnClickListener {
-            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+            if(!TextUtils.isEmpty(binding.editTextTextPersonName.text.toString())){
+                val bundle : Bundle = bundleOf("user_input" to binding.editTextTextPersonName.text.toString())
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment,bundle)
+            }else{
+                Toast.makeText(activity,"Please insert your name",Toast.LENGTH_SHORT).show()
+            }
         }
 
         return binding.root
--- app/src/main/java/com/example/navigationcomponentexample/SecondFragment.kt
+++ app/src/main/java/com/example/navigationcomponentexample/SecondFragment.kt
@@ -1,5 +1,6 @@
 package com.example.navigationcomponentexample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,12 +13,17 @@ import com.example.navigationcomponentexample.databinding.FragmentSecondBinding
 class SecondFragment : Fragment() {
     private lateinit var binding : FragmentSecondBinding
 
+    @SuppressLint("UseRequireInsteadOfGet")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second, container, false)
+
+        var input : String? = arguments!!.getString("user_input")
+        binding.textView.text = input.toString()
+
         return binding.root
     }
 
